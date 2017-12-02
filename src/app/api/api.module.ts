279449cc6881d9824a8a0af75d23defa38564a84
Aiden <chenscrocker@hotmail.com>
2017-12-02 17:25:39 +0800

    新增接口页面的header部分代码

--- src/app/api/api.module.ts
+++ src/app/api/api.module.ts
@@ -3,6 +3,7 @@ import { SharedModule } from '../shared/shared.module';
 
 import { ApiRoutingModule } from './api-routing.module';
 import { ApiComponent } from './api.component';
+import { ApiHeaderComponent } from './api-header/api-header.component';
 import { ApiListComponent } from './api-list/api-list.component';
 import { ApiDetailComponent } from './api-detail/api-detail.component';
 
@@ -13,6 +14,7 @@ import { ApiDetailComponent } from './api-detail/api-detail.component';
   ],
   declarations: [
     ApiComponent,
+    ApiHeaderComponent,
     ApiListComponent,
     ApiDetailComponent
   ]
